d00919bfec43d5ce91844a15ae841f276fc47f2c
Prakash <vontedduprakash@gmail.com>
2026-03-22 21:56:28 +0530

    UC5 implementation

--- ArrayList.java
+++ ArrayList.java
@@ -1,6 +1,11 @@
-public class HelloApp{
-	public static void main(string[] args){
-		String name = args[0];
-		System.out.println("Hello, " + name + "!");
+public class HelloApp {
+	public static void main(string[] args) {
+		if (args.length > 0) {
+			for (int i = 0; i < args.length; i++) {
+				System.out.println("Hello, " + args[i] + "!");
+			}
+		} else {
+			System.out.println("Hello, World!");
+		}
 	}
-}	
+}
